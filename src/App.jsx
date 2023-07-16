5ae52edcf33d6ae37b37d771eb71d628c559225f
Beyond77 <javierrubin22@gmail.com>
2023-07-16 17:17:05 -0600

    add animations

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
             <h1 className="header">Rick And Morty Finder</h1>
             <div>
                 <form className="search-form" onSubmit={handleSubmit} >
-                    <input type="text" name="Search" id="Search" placeholder="Character" onChange={ (e) => dispatch(setCharacterSearch({characterSearch: e.target.value})) } value={characterSearch} />
+                    <input type="text" name="Search" id="Search" placeholder="Search" onChange={ (e) => dispatch(setCharacterSearch({characterSearch: e.target.value})) } value={characterSearch} />
                 </form>
                 
                 <Pagination page={page}/>
